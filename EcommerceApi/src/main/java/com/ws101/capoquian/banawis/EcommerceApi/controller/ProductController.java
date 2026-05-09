19f107b841afcb756fffe7d4a3cd34bc826332b5
capoquianjojo552-dotcom <capoquianjojo552@gmail.com>
2026-05-09 13:24:23 +0800

    Lab 8: Setup JPA and MySQL connection

--- EcommerceApi/src/main/java/com/ws101/capoquian/banawis/EcommerceApi/controller/ProductController.java
+++ EcommerceApi/src/main/java/com/ws101/capoquian/banawis/EcommerceApi/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.ws101.capoquian.banawis.EcommerceApi.controller;
 
 import com.ws101.capoquian.banawis.EcommerceApi.model.Product;
 import com.ws101.capoquian.banawis.EcommerceApi.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +14,6 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @Autowired 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
